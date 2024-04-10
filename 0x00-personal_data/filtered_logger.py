ff570734571d650e883ba549009909bce352f2ec
reem <tarekreem950@gmail.com>
2024-04-11 01:33:44 +0200

    fix errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""
-logging module
-"""
+"""logging module"""
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str):
+                 redaction: str, message: str, separator: str) -> str:
     """filter function"""
     for i in fields:
         message = re.sub(f"(?<={i}=)(.*?)(?={separator})", redaction, message)
