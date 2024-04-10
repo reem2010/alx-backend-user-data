e04f8f9bff7394613de2482a6fb05fc2d6aa6a87
reem <tarekreem950@gmail.com>
2024-04-11 01:34:42 +0200

    fix errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: list,
+def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """filter function"""
     for i in fields:
