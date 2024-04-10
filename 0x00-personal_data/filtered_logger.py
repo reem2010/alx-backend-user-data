59cc51f187bf1501ac90c290397771ff718b2854
reem <tarekreem950@gmail.com>
2024-04-11 01:45:00 +0200

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """logging module"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -10,3 +11,20 @@ def filter_datum(fields: List[str],
     for i in fields:
         message = re.sub(f"(?<={i}=)(.*?)(?={separator})", redaction, message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format method"""
+        pass
+
