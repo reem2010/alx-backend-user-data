f0f0a74d976131dd780890aee78ba47c6e281151
reem <tarekreem950@gmail.com>
2024-04-12 00:11:53 +0200

    encrypt pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     """hash function"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """valid passfunction"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
