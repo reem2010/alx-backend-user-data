02de84de1e919d60cb3c285fec0dabf768656b90
reem <tarekreem950@gmail.com>
2024-04-15 20:37:00 +0200

    require_auth mrthod

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,13 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth method"""
-        return False
+        if (not (path) or not(excluded_paths) or len(excluded_paths) == 0):
+            return True
+        if (path[-1] != '/'):
+            path = path + "/"
+        if (path in excluded_paths):
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """auth method"""
