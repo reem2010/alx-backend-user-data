671e963277d1d8c8240ad127c1dc0650f4a68cd6
reem <tarekreem950@gmail.com>
2024-04-17 00:04:07 +0200

    last tak

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,8 +16,11 @@ class Auth():
             return True
         if (path[-1] != '/'):
             path = path + "/"
-        if (path in excluded_paths):
-            return False
+        for var in excluded_paths:
+            if (var[-1] == "*") and var[:-1] in path:
+                return False
+            if (path == var):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
