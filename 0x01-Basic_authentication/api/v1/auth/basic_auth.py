a5c2187f79be9ebfb03cd02a762ced01ece2da2c
reem <tarekreem950@gmail.com>
2024-04-16 02:23:03 +0200

    User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@ module for basic auth
 """
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -47,3 +48,19 @@ class BasicAuth(Auth):
             return (None, None)
         res = decoded_base64_authorization_header.split(":")
         return tuple(res)
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> User:
+        """user object method"""
+        if(not(user_email) or type(user_email) != str):
+            return None
+        if(not(user_pwd) or type(user_pwd) != str):
+            return None
+        user = User.search({"email": user_email})
+        if(len(user) == 0):
+            return None
+        user = user[0]
+        if (not(user.is_valid_password(user_pwd))):
+            return None
+        return user
