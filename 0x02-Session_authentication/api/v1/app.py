2b705af4ea58b7785451871c54b91a0317a16c79
reem <tarekreem950@gmail.com>
2024-04-18 20:13:47 +0200

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,7 +32,8 @@ def before_request():
         return None
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if (not(auth.require_auth(request.path, excluded_paths))):
         return
     if (not(auth.authorization_header(request))):
