80384a08e64bf714a48a76790189c430341d9fdd
reem <tarekreem950@gmail.com>
2024-04-18 20:34:03 +0200

    current user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ module for session auth
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -27,3 +28,11 @@ class SessionAuth(Auth):
         if(type(session_id) != str):
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """RETURN CURRENT USER"""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_by_session_id(session_id)
+        if (not(user_id)):
+            return None
+        return User.get(user_id)
