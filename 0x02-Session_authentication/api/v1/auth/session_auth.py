6da86f5519323561a76b20f7538ecc81fabefef1
reem <tarekreem950@gmail.com>
2024-04-18 19:35:40 +0200

    create session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,4 @@ class SessionAuth(Auth):
             return None
         session_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
