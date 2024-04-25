b790a9f5c440f7069b21ab1346dc837d5d408a3b
reem <tarekreem950@gmail.com>
2024-04-25 02:42:17 +0200

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
@@ -42,8 +42,9 @@ class DB:
 
     def find_user_by(self, **kwargs):
         """find user"""
+        if (not kwargs):
+            raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).all()
         if len(user) == 0:
             raise NoResultFound
         return user[0]
-
