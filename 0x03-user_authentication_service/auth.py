b15fa66539036d939aecc2f0716a364287a324f0
reem <tarekreem950@gmail.com>
2024-04-25 05:08:08 +0200

    task16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,6 +61,16 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """reset password"""
+        try:
+            self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            setattr(User, 'reset_token', token)
+            return token
+        except Exception as e:
+            raise ValueError
+
 
 def _generate_uuid() -> str:
     """generate uuid"""
